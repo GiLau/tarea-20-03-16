f13b84955a9ae959768ca165694145baec043fb8
gilau <joyliss_baby_2211@hotmail.com>
2016-03-20 17:50:19 -0500

    tarea de Gina Crespin  y Laura Perero

---
+++ cuentos.js
@@ -0,0 +1,28 @@
+console.log("Inicio de programa");
+/*alert("Hola JavaScript");*/
+/*var nombre= prompt("Cual es tu nombre");
+document.write ("<h1> su nombre es:"  +nombre+"</h1>");
+/*document.write("<h1>Hola JavaScript!</h1>");
+alert("gracias por visitar");
+
+var equipo= prompt("cual es tu equipo favorito?");
+console.log("el equipo es:" +equipo);
+
+var clave = prompt("Hola Usuario, Ingrese su clave en minusculas!");
+console.log("La longitud de la clave es: " + clave.length);
+console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
+console.log("El valor original de la clave es: " +  clave);*/
+
+
+/*console.log("fin de programa");
+alert("cuento");*/
+
+var sust= prompt("sustantivo:");
+var adj= prompt("adjetivo");
+var verb= prompt("verbo");
+document.write("<h1> el "+sust+" "+adj+" queria "+verb+" en el patio</h1>");
+
+document.write("<h1>  queria "+verb+" el "+adj+" "+sust+" en el patio</h1>");
+
+document.write("<h1> en el patio  queria "+verb+"   el "+sust+" "+adj+"  </h1>");
+
