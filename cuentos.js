a9d898a82b08eab2265afa0a94e8106dec526f7d
gilau <joyliss_baby_2211@hotmail.com>
2016-03-20 17:51:46 -0500

    tarea de Gina  y Laura

--- cuentos.js
+++ cuentos.js
@@ -1,21 +1,5 @@
-console.log("Inicio de programa");
-/*alert("Hola JavaScript");*/
-/*var nombre= prompt("Cual es tu nombre");
-document.write ("<h1> su nombre es:"  +nombre+"</h1>");
-/*document.write("<h1>Hola JavaScript!</h1>");
-alert("gracias por visitar");
+/*console.log("Inicio de programa");*/
 
-var equipo= prompt("cual es tu equipo favorito?");
-console.log("el equipo es:" +equipo);
-
-var clave = prompt("Hola Usuario, Ingrese su clave en minusculas!");
-console.log("La longitud de la clave es: " + clave.length);
-console.log("TA-DA!!! Su clave es: " + clave.toUpperCase());
-console.log("El valor original de la clave es: " +  clave);*/
-
-
-/*console.log("fin de programa");
-alert("cuento");*/
 
 var sust= prompt("sustantivo:");
 var adj= prompt("adjetivo");
